f30817fda3bec6a6eb442acc78e021743c4b8a45
Nilton OS <jniltinho@gmail.com>
2024-02-12 21:17:50 -0300

    Add Base Layout

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"hello-echo/templates"
+	"hello-echo/view"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,10 +17,10 @@ func HelloName(c echo.Context) error {
 	title := "Hello:" + c.Param("name")
 	name := c.Param("name")
 	//return templates.Index(title, name).Render(context.Background(), c.Response().Writer)
-	return render(c, templates.Index(title, name))
+	return render(c, view.Index(title, name))
 }
 
 func Pong(c echo.Context) error {
 	//return templates.Index("Pong", "FuncPong").Render(context.Background(), c.Response().Writer)
-	return render(c, templates.Index("Pong", "FuncPong"))
+	return render(c, view.Index("Pong", "FuncPong"))
 }
